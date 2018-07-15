f58036d29e88cf06936b125398a8e92bc199c756
Welington Gadelha <gadelha.welington@gmail.com>
2018-07-14 20:39:40 -0400

    Commit dos primeiros dados

---
+++ ComCity/ComCity/Models/Modelo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComCity.Models
+{
+    public class Modelo
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+    }
+}
